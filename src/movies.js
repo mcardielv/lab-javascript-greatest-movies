6e8dd9046029d64019208cb5c3d0c32cd3b35ec7
Miquel Cardiel <mcardielv@hotmail.com>
2022-09-14 01:45:47 +0200

    iteration 3

--- src/movies.js
+++ src/movies.js
@@ -3,31 +3,50 @@ const movies = require("./data.js");
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {
-  const rawList = moviesArray.map((movie) => movie.director);
+// function getAllDirectors(moviesArray) {
+//   const rawList = moviesArray.map((movie) => movie.director);
 
-  const cleanList = rawList.filter((director, index) => {
-    return rawList.indexOf(director) === index;
-  });
-  return cleanList;
-}
-console.log(`1.A list of director of this data is ${getAllDirectors(movies)}`);
+//   const cleanList = rawList.filter((director, index) => {
+//     return rawList.indexOf(director) === index;
+//   });
+//   return cleanList;
+// }
+// console.log(`1.A list of director of this data is ${getAllDirectors(movies)}`);
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {
-  const steven = moviesArray.filter((movieItem) => {
-    return movieItem.director === "Steven Spielberg";
-  });
 
-  const dramaMovies = steven.filter((spielbergMovies) => {
-    return spielbergMovies.genre.includes("Drama");
-  });
-  return dramaMovies;
-}
-console.log(howManyMovies(movies));
+//ARROW FUNCTION
+// function howManyMovies(moviesArray) {
+//   const steven = moviesArray.filter((movieItem) => {
+//     return movieItem.director === "Steven Spielberg";
+//   });
+
+//   const dramaMovies = steven.filter((spielbergMovies) => {
+//     return spielbergMovies.genre.includes("Drama");
+//   });
+//   return dramaMovies;
+// }
+// console.log(howManyMovies(movies));
+
+//REGULAR FUNCTION
+// function howManyMovies(moviesArray) {
+//   const spielberg = howManyMovies.filter(moviesArray);
+
+//   return spielberg.director === "Steven Spielberg";
+// }
+// howManyMovies(movies);inacabada :(
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+  let totalSum = 0;
+  const scores = moviesArray.map((movie) => movie.score);
+  for (let i = 0; i < scores.length; i++) {
+    totalSum = totalSum + scores[i];
+  }
+  const numFixed = totalSum / moviesArray.length;
+  return numFixed.toFixed(2);
+}
+console.log(scoresAverage(movies));
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {}
